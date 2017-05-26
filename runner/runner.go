b4dc054d35bb653f4ce9054b2f87881400729d7a
bejczib <bejczi.balint@gmail.com>
2017-05-26 11:48:32 +0200

    feat: pipe stdin/out/err

--- runner/runner.go
+++ runner/runner.go
@@ -1,6 +1,11 @@
 package runner
 
-import "os/exec"
+import (
+	"io"
+	"log"
+	"os"
+	"os/exec"
+)
 
 type Runner interface {
 	Run()
@@ -16,6 +21,29 @@ func New(port string, name string, args []string) Runner {
 }
 
 func (sp *subProcess) Run() {
+
 	cmd := exec.Command(sp.name, sp.args...)
-	cmd.Start()
+
+	stdout, err := cmd.StdoutPipe()
+	checkError(err)
+	stderr, err := cmd.StderrPipe()
+	checkError(err)
+	stdin, err := cmd.StderrPipe()
+	checkError(err)
+
+	err = cmd.Start()
+	checkError(err)
+
+	defer cmd.Wait()
+
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
+	go io.Copy(os.Stdin, stdin)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
