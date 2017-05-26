e5d020e1fca45c095c1534e3b5210027e89b897d
Adam Luzsi <adamluzsi@gmail.com>
2017-05-26 10:34:57 +0200

    update request to return path and query parts

--- request.go
+++ request.go
@@ -1,5 +1,7 @@
 package escher
 
+import "net/url"
+
 type RequestHeaders [][2]string
 
 type Request struct {
@@ -7,4 +9,33 @@ type Request struct {
 	Url     string         `json:"url"`
 	Headers RequestHeaders `json:"headers"`
 	Body    string         `json:"body"`
+}
+
+func (r Request) Path() (string, error) {
+	url, err := url.Parse(r.Url)
+
+	if err != nil {
+		return "", err
+	}
+
+	return url.Path, err
+}
+
+type QueryParts [][2]string
+
+func (r Request) QueryParts() (QueryParts, error) {
+	url, err := url.Parse(r.Url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	queryParts := make(QueryParts, 0)
+	for key, values := range url.Query() {
+		for _, value := range values {
+			queryParts = append(queryParts, [2]string{key, value})
+		}
+	}
+
+	return queryParts, nil
 }
