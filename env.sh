b72557d061460929a4dff15011e1f7586e490200
Adam Luzsi <adamluzsi@gmail.com>
2017-06-13 12:54:31 +0200

    remove test cases from the repo

--- env.sh
+++ env.sh
@@ -1,1 +1,15 @@
-export TEST_SUITE_PATH="`pwd`"
+
+testCasesDirectoryName="test_cases"
+testCaseRepoCloneDirector="`pwd`"
+testCaseDirectoryPath="$testCaseRepoCloneDirector/$testCasesDirectoryName"
+
+if [ ! -d "$testCaseDirectoryPath" ];then
+    git clone https://github.com/adamluzsi/escher-test-suite $testCasesDirectoryName
+fi
+
+cd $testCasesDirectoryName
+git checkout -- .
+git pull
+cd $testCaseRepoCloneDirector
+
+export TEST_SUITE_PATH="$testCaseDirectoryPath/test_cases"
