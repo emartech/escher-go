a1b86732c18fc52ea9a859ac34d34f28c1817861
Adam Luzsi <adamluzsi@gmail.com>
2017-07-20 12:43:00 +0200

    ENV Values set from ESCHER prefixed env variables

--- config/env.go
+++ config/env.go
@@ -10,15 +10,63 @@ const (
 )
 
 func NewFromENV() (Config, error) {
-	config := Config{}
+	c := Config{}
 
-	jsonString, configJSONStringIsPresent := os.LookupEnv(escherConfigEnv)
+	err := setByConfigJSON(&c)
 
-	if configJSONStringIsPresent {
-		json.Unmarshal([]byte(jsonString), &config)
+	if err != nil {
+		return c, err
 	}
 
-	setDefaults(&config)
+	setValuesFromDifferentEnv(&c)
 
-	return config, nil
+	SetDefaults(&c)
+
+	return c, nil
+}
+
+func setByConfigJSON(c *Config) error {
+	var err error
+
+	data, configJSONStringIsPresent := os.LookupEnv(escherConfigEnv)
+
+	if configJSONStringIsPresent {
+		err = json.Unmarshal([]byte(data), c)
+	}
+
+	return err
+}
+
+func setValuesFromDifferentEnv(c *Config) {
+
+	algoPrefix, isGiven := os.LookupEnv("ESCHER_ALGO_PREFIX")
+	if isGiven && c.AlgoPrefix == "" {
+		c.AlgoPrefix = algoPrefix
+	}
+
+	hashAlgo, isGiven := os.LookupEnv("ESCHER_HASH_ALGO")
+	if isGiven && c.HashAlgo == "" {
+		c.HashAlgo = hashAlgo
+	}
+
+	vendorKey, isGiven := os.LookupEnv("ESCHER_VENDOR_KEY")
+	if isGiven && c.VendorKey == "" {
+		c.VendorKey = vendorKey
+	}
+
+	authHeaderName, isGiven := os.LookupEnv("ESCHER_AUTH_HEADER_NAME")
+	if isGiven && c.AuthHeaderName == "" {
+		c.AuthHeaderName = authHeaderName
+	}
+
+	dateHeaderName, isGiven := os.LookupEnv("ESCHER_DATE_HEADER_NAME")
+	if isGiven && c.DateHeaderName == "" {
+		c.DateHeaderName = dateHeaderName
+	}
+
+	credentialScope, isGiven := os.LookupEnv("ESCHER_CREDENTIAL_SCOPE")
+	if isGiven && c.CredentialScope == "" {
+		c.CredentialScope = credentialScope
+	}
+
 }
--- config/env_test.go
+++ config/env_test.go
@@ -36,7 +36,7 @@ func TestNewFromENV_ConfigJSONIsPresentInTheEnv(t *testing.T) {
 
 }
 
-func TestNewFromENV_ValidJSONIsPresentButWithMissingValues(t *testing.T) {
+func TestNewFromENV_ValidJSONIsPresentButOnlyCredentialScopeIsProvided(t *testing.T) {
 	defer SetEnvForTheTest(t, "ESCHER_CONFIG", `{"credentialScope": "a/b/c"}`)()
 
 	config, err := config.NewFromENV()
@@ -52,4 +52,27 @@ func TestNewFromENV_ValidJSONIsPresentButWithMissingValues(t *testing.T) {
 	assert.Equal(t, "X-Escher-Date", config.DateHeaderName)
 	assert.Equal(t, "a/b/c", config.CredentialScope)
 
+}
+
+func TestNewFromENV_EveryValueIsProvidedInEnvVariables(t *testing.T) {
+	defer UnsetEnvForTheTest(t, "ESCHER_CONFIG")()
+	defer SetEnvForTheTest(t, "ESCHER_ALGO_PREFIX", "ALGO_PREFIX")()
+	defer SetEnvForTheTest(t, "ESCHER_HASH_ALGO", "HASH_ALGO")()
+	defer SetEnvForTheTest(t, "ESCHER_VENDOR_KEY", "VENDOR_KEY")()
+	defer SetEnvForTheTest(t, "ESCHER_AUTH_HEADER_NAME", "AUTH_HEADER_NAME")()
+	defer SetEnvForTheTest(t, "ESCHER_DATE_HEADER_NAME", "DATE_HEADER_NAME")()
+	defer SetEnvForTheTest(t, "ESCHER_CREDENTIAL_SCOPE", "CREDENTIAL_SCOPE")()
+
+	config, err := config.NewFromENV()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "ALGO_PREFIX", config.AlgoPrefix)
+	assert.Equal(t, "HASH_ALGO", config.HashAlgo)
+	assert.Equal(t, "VENDOR_KEY", config.VendorKey)
+	assert.Equal(t, "AUTH_HEADER_NAME", config.AuthHeaderName)
+	assert.Equal(t, "DATE_HEADER_NAME", config.DateHeaderName)
+	assert.Equal(t, "CREDENTIAL_SCOPE", config.CredentialScope)
 }
