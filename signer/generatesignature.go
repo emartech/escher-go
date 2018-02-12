c12d41274a788b96f94d4eb2ec88a4c4cd5547c2
Adam Luzsi <adamluzsi@gmail.com>
2018-02-12 08:42:31 +0100

    add more debug prints

--- signer/generatesignature.go
+++ signer/generatesignature.go
@@ -1,10 +1,15 @@
 package signer
 
-import "github.com/EscherAuth/escher/request"
+import (
+	"github.com/EscherAuth/escher/debug"
+	"github.com/EscherAuth/escher/request"
+)
 
 // TODO add more test to have explicit tests for this not just implicit
 func (s *signer) GenerateSignature(r request.Interface, headersToSign []string) string {
 	var stringToSign = s.getStringToSign(r, headersToSign)
 	var signingKey = s.calculateSigningKey()
+	debug.Println("GenerateSignature")
+	debug.Println(stringToSign, signingKey)
 	return s.calculateSignature(stringToSign, signingKey)
 }
