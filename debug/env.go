738d145ed197db08782b95c13d339b971840ac8f
Adam Luzsi <adamluzsi@gmail.com>
2018-02-12 08:26:05 +0100

    add an extra warning to the debug mod enable

--- debug/env.go
+++ debug/env.go
@@ -1,11 +1,25 @@
 package debug
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 var enabled bool
 
+const warningMessage = `
+WARNING: ESCHER_DEBUG is enabled!
+This should be only used in development for escher related problems debugging.
+With this sensitive information could be logged to the STDOUT!
+Use with extra caution
+`
+
 func init() {
 	_, isSet := os.LookupEnv("ESCHER_DEBUG")
+
+	if isSet {
+		fmt.Println(warningMessage)
+	}
 
 	enabled = isSet
 }
